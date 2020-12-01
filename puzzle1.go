407d36f8bf11fa3f70ca47f9211edf0a30931b43
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2020-12-01 12:37:16 +0100

    Day 1 Part 2

--- puzzle1.go
+++ puzzle1.go
@@ -28,3 +28,27 @@ func puzzle1(input []int) int {
 	}
 	panic("puzzle1 result not found")
 }
+
+func Puzzle1b() string {
+	data, _ := ioutil.ReadFile("input/input01")
+	strs := strings.Split(strings.TrimRight(string(data), "\n"), "\n")
+	input := make([]int, len(strs))
+	for i, s := range strs {
+		input[i], _ = strconv.Atoi(s)
+	}
+	return fmt.Sprintf("Puzzle1b result: %d", puzzle1b(input))
+}
+
+func puzzle1b(input []int) int {
+	sum := 2020
+	for _, i := range input {
+		for _, j := range input {
+			for _, k := range input {
+				if i+j+k == sum {
+					return i * j * k
+				}
+			}
+		}
+	}
+	panic("puzzle1 result not found")
+}
--- puzzle1_test.go
+++ puzzle1_test.go
@@ -26,3 +26,25 @@ func ExamplePuzzle1() {
 
 	// Output: Puzzle1 result: 1019571
 }
+
+func TestPuzzle1b(t *testing.T) {
+	testinput := []int{
+		1721,
+		979,
+		366,
+		299,
+		675,
+		1456,
+	}
+	result := puzzle1b(testinput)
+	expect := 241861950
+	if result != expect {
+		t.Fatalf("incorrect output: expected %d, got %d", expect, result)
+	}
+}
+
+func ExamplePuzzle1b() {
+	fmt.Println(Puzzle1b())
+
+	// Output: Puzzle1b result: 100655544
+}
