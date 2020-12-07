1423e1ec41b071f8de66f5f22b9ad95a5dbb102a
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2020-12-07 12:23:32 +0100

    Day 7 Part 2

--- puzzle7.go
+++ puzzle7.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -48,4 +49,54 @@ func puzzle7(data []string) int {
 func Puzzle7() int {
 	data := readFile("input/input07")
 	return puzzle7(data)
+}
+
+type bag struct {
+	count int
+	name  string
+}
+
+func puzzle7b(data []string) int {
+	subject := "shiny gold"
+
+	outerExpr := regexp.MustCompile(`^(\w+ \w+) bags contain (.+)\.$`)
+	innerExpr := regexp.MustCompile(`^(\d+) (\w+ \w+) bags?$`)
+
+	bags := make(map[string][]bag)
+	for _, line := range data {
+		matches := outerExpr.FindStringSubmatch(line)
+		if matches == nil {
+			panic("unexpected line: " + line)
+		}
+		outer := matches[1]
+		inners := strings.Split(matches[2], ", ")
+		for _, inner := range inners {
+			if inner == "no other bags" {
+				continue
+			}
+			smatches := innerExpr.FindStringSubmatch(inner)
+			if smatches == nil {
+				panic("unexpected subline: " + inner)
+			}
+			count, _ := strconv.Atoi(smatches[1])
+			bags[outer] = append(bags[outer], bag{count, smatches[2]})
+		}
+	}
+
+	var count func(bag string) int
+	count = func(bag string) int {
+		var c int
+		for _, b := range bags[bag] {
+			c += b.count + b.count*count(b.name)
+		}
+		return c
+	}
+
+	result := count(subject)
+	return result
+}
+
+func Puzzle7b() int {
+	data := readFile("input/input07")
+	return puzzle7b(data)
 }
