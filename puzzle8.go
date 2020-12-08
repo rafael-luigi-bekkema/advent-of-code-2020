ed9615244512b9e1eb2c9939ab80bb705d826815
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2020-12-08 11:23:28 +0100

    Day 8 Part 2

--- puzzle8.go
+++ puzzle8.go
@@ -33,3 +33,64 @@ func Puzzle8() int {
 	data := readFile("input/input08")
 	return puzzle8(data)
 }
+
+func p8run(data []string) (int, bool) {
+	var acc, p int
+	hist := make(map[int]bool)
+	for {
+		if hist[p] {
+			return acc, false
+		}
+		hist[p] = true
+		if p == len(data) {
+			return acc, true
+		}
+
+		line := data[p]
+		var ins string
+		var n int
+		fmt.Sscanf(line, "%s %d", &ins, &n)
+
+		switch ins {
+		case "nop":
+			p++
+		case "acc":
+			acc += n
+			p++
+		case "jmp":
+			p += n
+		}
+	}
+}
+
+func puzzle8b(data []string) int {
+	for i := 0; i < len(data); i++ {
+		line := data[i]
+		var ins string
+		var n int
+		fmt.Sscanf(line, "%s %d", &ins, &n)
+		if ins == "acc" {
+			continue
+		}
+
+		ndata := append([]string{}, data...)
+		var nins string
+		if ins == "nop" {
+			nins = "jmp"
+		} else {
+			nins = "nop"
+		}
+		ndata[i] = fmt.Sprintf("%s %+d", nins, n)
+
+		acc, ok := p8run(ndata)
+		if ok {
+			return acc
+		}
+	}
+	panic("no answer found")
+}
+
+func Puzzle8b() int {
+	data := readFile("input/input08")
+	return puzzle8b(data)
+}
--- puzzle8_test.go
+++ puzzle8_test.go
@@ -28,3 +28,26 @@ func ExamplePuzzle8() {
 
 	// Output: 1179
 }
+
+func TestPuzzle8b(t *testing.T) {
+	data := strings.Split(`nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6`, "\n")
+	expect := 8
+	result := puzzle8b(data)
+	if result != expect {
+		t.Fatalf("expected %d, got %d", expect, result)
+	}
+}
+
+func ExamplePuzzle8b() {
+	fmt.Println(Puzzle8b())
+
+	// Output: 1089
+}
