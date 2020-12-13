012f9c1b5737292aea31a74d60e7b00293e12c78
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2020-12-13 13:48:15 +0100

    Day 13 Part 2

--- puzzle13.go
+++ puzzle13.go
@@ -31,3 +31,56 @@ func Puzzle13() int {
 	data := readFile("input/input13")
 	return puzzle13(data)
 }
+
+func puzzle13b(data string) int {
+	var buses []int
+	for _, b := range strings.Split(data, ",") {
+		bus, _ := strconv.Atoi(b)
+		buses = append(buses, bus)
+	}
+
+	var t int
+	matches := map[int]int{
+		0: buses[0],
+	}
+	var addPoss int
+	add := buses[0]
+	for {
+		t += add
+
+		found := true
+		for i, bus := range buses {
+			if bus == 0 || i == 0 {
+				continue
+			}
+			if (t+i)%bus != 0 {
+				found = false
+				break
+			} else {
+				// This is the important part to make it
+				// fast enough for the full input.
+				// When a bus i > 0 lines up the first time, store t.
+				// When it lines up the second time, make the
+				// skip we are doing t-prevt.
+				if prevt, ok := matches[i]; !ok {
+					matches[i] = t
+				} else if addPoss < i {
+					add = t - prevt
+					addPoss = i
+					matches[i] = t
+				}
+			}
+		}
+
+		if found {
+			break
+		}
+	}
+
+	return t
+}
+
+func Puzzle13b() int {
+	data := readFile("input/input13")
+	return puzzle13b(data[1])
+}
--- puzzle13_test.go
+++ puzzle13_test.go
@@ -21,3 +21,34 @@ func ExamplePuzzle13() {
 
 	// Output: 259
 }
+
+func TestPuzzle13b(t *testing.T) {
+	tt := []struct {
+		data   string
+		expect int
+	}{
+		{"7,13,x,x,59,x,31,19", 1068781},
+		{"17,x,13,19", 3417},
+		{"67,7,59,61", 754018},
+		{"67,x,7,59,61", 779210},
+		{"67,7,x,59,61", 1261476},
+		{"1789,37,47,1889", 1202161486},
+	}
+
+	// t * 7 == t * 18 + 1
+
+	for i, tc := range tt {
+		t.Run(fmt.Sprintf("Test %d", i+1), func(t *testing.T) {
+			result := puzzle13b(tc.data)
+			if result != tc.expect {
+				t.Fatalf("expected %d, got %d", tc.expect, result)
+			}
+		})
+	}
+}
+
+func ExamplePuzzle13b() {
+	fmt.Println(Puzzle13b())
+
+	// Output: 210612924879242
+}
