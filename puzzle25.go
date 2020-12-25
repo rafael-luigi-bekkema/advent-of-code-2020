bd6a4c19c6955abc59a629ece375cdf9768d5158
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2020-12-25 12:39:55 +0100

    Optimize more

--- puzzle25.go
+++ puzzle25.go
@@ -1,41 +1,16 @@
 package main
 
-func findLoops(key int) int {
-	value := 1
-	loop := 0
-	subject := 7
-	for {
-		value = (subject * value) % 20201227
-		loop++
-		if value == key {
-			break
-		}
-		if loop >= 100_000_000 {
-			panic("too many loops")
-		}
+func puzzle25(cardKey, doorKey int) int {
+	loops := 0
+	for value := 1; value != cardKey; loops++ {
+		value = (7 * value) % 20201227
 	}
-	return loop
-}
-func doLoops(subject, loops int) int {
+
 	value := 1
 	for loop := 1; loop <= loops; loop++ {
-		value = (subject * value) % 20201227
+		value = (doorKey * value) % 20201227
 	}
 	return value
-}
-
-func puzzle25(cardKey, doorKey int) int {
-	cardLoops := findLoops(cardKey)
-	doorLoops := findLoops(doorKey)
-
-	encKey1 := doLoops(cardKey, doorLoops)
-	encKey2 := doLoops(doorKey, cardLoops)
-
-	if encKey1 != encKey2 {
-		panic("keys are not equal")
-	}
-
-	return encKey1
 }
 
 func (s *Solutions) Puzzle25() int {
